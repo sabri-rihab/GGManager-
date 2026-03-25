18389137d98fec4a90e26ce74b71ab1048dc287d
Salma GTU <salmajaddar7@gmail.com>
2026-03-25 09:22:24 +0100

    fixer le problem pour la table matches_games

---
+++ database/migrations/2026_03_25_081955_create_match_games_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('match_games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tournament_id')->constrained()->onDelete('cascade');
+            $table->foreignId('player1_id')->nullable()->constrained('users');
+            $table->foreignId('player2_id')->nullable()->constrained('users');
+            $table->integer('score_p1')->nullable();
+            $table->integer('score_p2')->nullable();
+            $table->foreignId('winner_id')->nullable()->constrained('users');
+            $table->foreignId('next_match_id')->nullable()->constrained('match_games');
+            $table->boolean('is_bye')->default(false);
+            $table->integer('round');
+            $table->integer('position');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('match_games');
+    }
+};
